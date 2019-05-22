067fbef3e60ae03d1e005493f21d6600c9d9524c
alex <alexboxxer@web.de>
2019-05-22 14:19:56 +0200

    final solution

--- HackerRank/src/javaIntroduction/IfElse/Solution.java
+++ HackerRank/src/javaIntroduction/IfElse/Solution.java
@@ -10,15 +10,33 @@ import java.util.regex.*;
 
 public class Solution {
 
-
-
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String WEIRD = "Weird";
+    private static final String NOT_WEIRED = "Not Weird";
 
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+        
+        /* 
+         * First decide if N is odd or even
+         * 
+         * 1 = odd and 0 is even
+         */
+        if ((N % 2) == 1) {
+        	System.out.print(WEIRD);
+        }
+        else {
+        	/*
+        	 * decide in which range N is
+        	 */
+        	if (N >= 6 && N <= 20) {
+        		System.out.print(WEIRD);
+        	}
+        	System.out.print(NOT_WEIRED);
+        }
     }
 }
 
