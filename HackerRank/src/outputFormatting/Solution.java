815a85fc40f0839297be4ee8be8b5d07d89d253f
alex <alexboxxer@web.de>
2019-05-22 17:07:53 +0200

    final solution

--- HackerRank/src/outputFormatting/Solution.java
+++ HackerRank/src/outputFormatting/Solution.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Solution {
 
 public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	System.out.println("================================");
-	for (int i=0;i<3;i++) {
-		String s1 = scan.next();
-		int x = scan.nextInt();
+    Scanner sc=new Scanner(System.in);
+    System.out.println("================================");
+    for(int i=0;i<3;i++)
+    {
+        String s1=sc.next();
+        int x=sc.nextInt();
         System.out.printf("%-15s%03d%n", s1, x);
-	}
-	System.out.println("================================");
+    }
+    System.out.println("================================");
 	}
 }
