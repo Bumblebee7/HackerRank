94b1ad7d3114cb0ca40ceab5490044fcc2e8ea19
alex <alexboxxer@web.de>
2019-05-23 22:06:55 +0200

    fixed a bug: numbers bigger than long caused an illegal input exception.

--- HackerRank/src/javaIntroduction/Datatypes/Solution.java
+++ HackerRank/src/javaIntroduction/Datatypes/Solution.java
@@ -7,34 +7,36 @@ public class Solution {
 	private static Scanner scan;
 	
 	public static void main(String[] args) {
-		 
-		try {
-			scan = new Scanner(System.in);
-			int times = scan.nextInt();
-			
-			for(int i=0;i<times;i++) {
-					long x = scan.nextLong();
-					
-					if(x>=-9223372036854775808L && x<=9223372036854775807L) {
-						System.out.printf("%d can be fitted in:%n", x);
-						System.out.println("* long");
-						if(x>=-2147483648 && x<=2147483647) {
-							System.out.println("* integer");
-							if(x>=-32768 && x<=32767) {
-								System.out.println("* short");
-								if(x>=-128 && x<=127) {
-									System.out.println("* byte");
-								}
-							}
-						}
-					}
-				}
-		} 
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		finally {
-			scan.close();
-		}
+	
+	    scan = new Scanner(System.in);
+	    int times = scan.nextInt();
+
+	    for(int i=0; i<times; i++) {
+
+	        try {
+	            long x = scan.nextLong();
+	            System.out.println(x + " can be fitted in:");
+	            if(x >= -128 && x <= 127) {
+	            	System.out.println("* byte");
+	            }
+	            if(x>=-32768 && x<=32767) {
+		            System.out.println("* short");
+	            }
+	            if(x >= Math.pow(-2,31) && x < Math.pow(2,31)) {
+	            	// between -2147483648 and 2147483647
+		            System.out.println("* int");
+	            }
+	            if(x >= Math.pow(-2,63) && x <= Math.pow(2,63)) {
+	            	// between -9223372036854775808L and 9223372036854775807L
+		            System.out.println("* long");
+	            }
+	        }
+	        catch(Exception e)
+	        {
+	            System.out.println(scan.next() + " can't be fitted anywhere.");
+	        }
+
+	    }
+	    scan.close();
 	}
 }
