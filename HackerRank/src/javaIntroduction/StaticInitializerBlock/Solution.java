de4d8589c6f51f89fcdbfd68669be85d711453ea
alex <alexboxxer@web.de>
2019-05-26 21:20:51 +0200

    Added Static Initializer Block

---
+++ HackerRank/src/javaIntroduction/StaticInitializerBlock/Solution.java
@@ -0,0 +1,37 @@
+package javaIntroduction.StaticInitializerBlock;
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	private static boolean flag;
+	private static int B, H;
+	
+	static {
+		try(Scanner scan = new Scanner(System.in)) {
+			B = scan.nextInt();
+			H = scan.nextInt();
+		}
+		
+		if(B < 0 || H < 0) {
+			flag = false;
+			/*
+			 * Use the following System.out.println in HackerRank, but the real Exception in the IDE
+			 */
+			System.out.println("java.lang.Exception: Breadth and height must be positive");
+			//throw new IllegalArgumentException("Breadth and height must be positive");
+		}
+		else {
+			flag = true;
+		}
+	}
+	
+	public static void main(String[] args) {
+		if(flag) {
+			int area = B * H;
+			System.out.println(area);
+		}
+
+	}
+
+}
