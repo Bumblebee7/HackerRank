2de86e4781dfc85ae497e434ed900cca9b207634
alex <alexboxxer@web.de>
2019-07-07 17:27:14 +0200

    Deleted the provided I/O Part

--- HackerRank/src/interviewPreparationKit/warmUp/RepeatedString/Solution.java
+++ HackerRank/src/interviewPreparationKit/warmUp/RepeatedString/Solution.java
@@ -31,25 +31,5 @@ public class Solution {
     	// 2 * 3 + 1
     	return numOfAs * factor + rest;
     }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String s = scanner.nextLine();
-
-        long n = scanner.nextLong();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        long result = repeatedString(s, n);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
 }
 
